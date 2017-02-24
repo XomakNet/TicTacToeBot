a1d529c11e88b5a73b3d4da9c15354589881a41b
Oleg Bulichev <obulichev@yandex.ru>
2017-02-24 16:47:58 +0300

    Code refactoring

---
+++ getAllLines.m
@@ -0,0 +1,12 @@
+function [ arrLineSegments ] = getAllLines( board )
+
+%curLine= struct ('player', '' , 'start_point', [0;0;0] , 'end_point', [0;0;0],'state', [0 0 0 0]);
+
+%board = zeros(4,4,4);
+
+%spatial diags + plane diags
+arrLineSegments = [planelDiagsHandler(board); spatialDiagsHandler(board)];
+%planeLines
+arrLineSegments = [arrLineSegments; planeLinesHandler(board)];
+end
+
--- planeDiagsHandler.m
+++ planeDiagsHandler.m
@@ -1,8 +1,57 @@
 function [ arrayOfSegmentLines ] = planeDiagsHandler( board )
 
+%for 'i' side
+j = 1;
+k = 1;
 for i = 1:size(board,1)
-    
+    while(j < size(board,2) && k < size(board,3))
+    %handler
+    j = j+1;
+    k = k+1;
+    end
+    j = 1;
+    k = size(board,3);
+    while(j < size(board,2) && k > 1)
+    %handler
+    j = j+1;
+    k = k-1;
+    end
 end
 
+%for 'j' side
+i = 1;
+k = 1;
+for j = 1:size(board,2)
+    while(i < size(board,2) && k < size(board,3))
+    %handler
+    i = i+1;
+    k = k+1;
+    end
+    i = 1;
+    k = size(board,3);
+    while(i < size(board,2) && k > 1)
+    %handler
+    i = i+1;
+    k = k-1;
+    end
+end
+
+%for 'i' side
+j = 1;
+k = 1;
+for i = 1:size(board,1)
+    while(j < size(board,2) && k < size(board,3))
+    %handler
+    j = j+1;
+    k = k+1;
+    end
+    j = 1;
+    k = size(board,3);
+    while(j < size(board,2) && k > 1)
+    %handler
+    j = j+1;
+    k = k-1;
+    end
+end
 end
 
