4e82e2e0ddc975166eb44348b16f24f6d4821f9c
Xomak <mail@xomak.net>
2017-02-25 18:06:26 +0300

    Modified simulateGame to get players' functions

--- simulateGame.m
+++ simulateGame.m
@@ -1,19 +1,17 @@
-function [ ] = simulateGame()
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+function [ field, state ] = simulateGame(firstPlayerFunction, secondPlayerFunction)
+%simulateGame Runs game between given player functions
+%   Returns game state at the end
 field = zeros(4, 4, 4);
 state = 0;
 while state == 0
-    move = playTTTT(field, 1)
+    move = firstPlayerFunction(field, 1);
     field(move(1), move(2), move(3)) = 1;
     state = getGameStateFromLines(getAllLines(field));
     if state == 0
-        move = playRandom(field, 2)
+        move = secondPlayerFunction(field, 2);
         field(move(1), move(2), move(3)) = 2;
         state = getGameStateFromLines(getAllLines(field));
     end
 end
-
-state
 end
 
