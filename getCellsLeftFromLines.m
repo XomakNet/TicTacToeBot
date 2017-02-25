154bd0af419babcd6fa132eddc02248c977a6447
Xomak <mail@xomak.net>
2017-02-25 20:51:28 +0300

    Fixed bugs with getCellsLeftFromLines, getGameStateFromLines and getAllLines

--- getCellsLeftFromLines.m
+++ getCellsLeftFromLines.m
@@ -2,14 +2,14 @@ function [ cellsToWin, cellsToLose ] = getCellsLeftFromLines( lines, currentPlay
 %getCellsLeftFromLines Returns numbers of cells left to win
 %  Two vectors have the following structure [n1 .. nm], where n{d} is number of lines, requring {d}-1 cells to become complete 
 
-if size(lines, 1) ~= 0
+if size(lines, 2) ~= 0
     
     fieldSize = 4;
     
     cellsToWin = zeros(1, fieldSize + 1);
     cellsToLose = zeros(1, fieldSize + 1);
 
-    for p=1:size(lines, 1)
+    for p=1:size(lines, 2)
         line = lines(p);
         population = sum(line.state);
         cellsToPopulate = fieldSize - population;
