159f1da1d85706151f8b2f6123c956500d59f8d5
Xomak <mail@xomak.net>
2017-02-27 20:01:20 +0300

    Fixed coefficients values, which were wrongly changed earlier
    Incorrect change in d84c279de9a6f0eaef880120d2425b750f5f4580

--- +BulichevDanilovFunction/private/estimateMovesForOneSide.m
+++ +BulichevDanilovFunction/private/estimateMovesForOneSide.m
@@ -2,7 +2,7 @@ function [ result ] = estimateMovesForOneSide( estimatesForCurrentPlayer, estima
 %estimateMovesForOneSide Estimates final move score, considering both odds
 % to win and odds to lose
 
-result = [estimatesForCurrentPlayer 1-estimatesForEnemy] * [0.6;0.4];
+result = [estimatesForCurrentPlayer 1-estimatesForEnemy] * [0.5;0.5];
 
 end
 
