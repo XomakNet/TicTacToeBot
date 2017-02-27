d84c279de9a6f0eaef880120d2425b750f5f4580
Oleg Bulichev <obulichev@yandex.ru>
2017-02-27 19:23:17 +0300

    refactored code, old methods except playTTTT moved in to package

---
+++ playTTTT.m
@@ -0,0 +1,7 @@
+function move = playTTTT(board, player)
+import BulichevDanilovFunction.*
+%PLAYTTTT Summary of this function goes here
+%   Detailed explanation goes here
+move = playTTTT(board, player);
+end
+
